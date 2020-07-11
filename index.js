526b85f04369898dc1c7c8bd9965cb9eef9c8547
DwiNugroho <adwinugroho1@gmail.com>
2020-07-11 12:21:35 +0700

    upgrade quote and misuh

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const serverOption = {
     qrRefreshS: 15,
     devtools: false,
     chromiumArgs: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox'
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
     ]
 }
 
@@ -26,7 +26,7 @@ serverOption['executablePath'] = '/Applications/Google Chrome.app/Contents/MacOS
 }
 
 const startServer = async (from) => {
-create('Imperial', serverOption)
+    create('Imperial', serverOption)
         .then(client => {
             console.log('[SERVER] Server Started!')
 
@@ -40,6 +40,9 @@ create('Imperial', serverOption)
                 msgHandler(client, message)
             })
         })
+        .catch(() => {
+            console.log('ERROR');
+        });
 }
 
 async function msgHandler (client, message) {
@@ -49,15 +52,52 @@ async function msgHandler (client, message) {
         const { id, pushname } = sender
         const { name } = chat
         const time = moment(t * 1000).format('DD/MM HH:mm:ss')
-        const commands = ['#sticker', '#halo', '#stiker', '#anjing']
+        const commands = ['#sticker', '#stiker' ,'#halo', '#misuh', 'kojok', '#quote', '#quotes']
         const cmds = commands.map(x => x + '\\b').join('|')
         const cmd = type === 'chat' ? body.match(new RegExp(cmds, 'gi')) : type === 'image' && caption ? caption.match(new RegExp(cmds, 'gi')) : ''
+        const quotes = [
+            `"Percuma kusiapkan dunia bila yang kamu inginkan cuma bapakku"`,
+            `"Nyari duit memang susah, yang gampang ya ninggalin sholat"`,
+            `"gapapa ga dapet kamu, yang penting dapet bapakmu"`,
+            `"lembabkan bibirmu, mulutmu kasar"`,
+            `"Namanya manusia hidup ya diselimuti masalah, kalo diselimuti wijen ya jadi onde onde aja"`,
+            `"Are you some kind of kambing, because you make my heart terombang ambing"`,
+            `"Kalo kamu nyari yang sesuai selera kamu, mungkin kamu cocoknya sama indomie"`,
+            `"Daripada melukai diri sendiri lebih baik mabok saja"`,
+            `"itu yang pacaran beda agama, Tuhannya berantem ga ya?"`,
+            `"Pengen ngebalas kelakuanmu tapi Aku sadar manusia tidak boleh jahat sama binatang"`,
+            `"Bayangin kamu dikick dari kartu keluarga cuma gara gara bapakmu gabut"`,
+            `"Semakin dewasa semakin gak pengen pamer kekayaan, pamer pacar, pamer gadget mahal. bukannya takut riya, cuma emang gak punya aja."`,
+            `"Kuntilanak aja dimana mana ketawa, masa lu yang sejenisnya cemberut mulu"`,
+            `"Kamu jangan main ke rumahku ya, keluargaku takut sama anjing"`,
+        ];
+        const misuh = [
+            'ANAK BABI',
+            'NGENTOTT',
+            'ASUU',
+            'JANCOKK',
+            'CANGKEMU BOSOK',
+            'COCOTE KOYOK TAEK',
+            'BACOD ANJENG',
+            'TAEKK',
+            'SILITT',
+            'KELAMIN JANTANN',
+            'DAJJAL',
+            'COCOTE NING TILIS',
+            'KEMEM',
+            'RAIMU GELEM TAK DUPAK?',
+            'GELOD AYOO',
+            'LAMBEMU ISO MENENG RAK',
+            'NDASMU',
+            'UTEKE NING TILIS',
+        ];
+
 
         if (cmd) {
             if (!isGroupMsg) console.log('[EXEC]', color(time, 'yellow'), color(cmd[0]), 'from', color(pushname))
             if (isGroupMsg) console.log('[EXEC]', color(time, 'yellow'), color(cmd[0]), 'from', color(pushname), 'in', color(name))
             const args = body.trim().split(' ')
-            switch (cmd[0]) {
+            switch (cmd[0].toLowerCase()) {
                 case '#sticker':
                     if (isMedia) {
                         const mediaData = await decryptMedia(message)
@@ -71,8 +111,33 @@ async function msgHandler (client, message) {
                         client.sendText(from, 'Kirim gambar pake hashtag #sticker GOBLOK !')
                     }
                     break
+                case '#stiker':
+                    if (isMedia) {
+                        const mediaData = await decryptMedia(message)
+                        const imageBase64 = `data:${mimetype};base64,${mediaData.toString('base64')}`
+                        await client.sendImageAsSticker(from, imageBase64)
+                    } else if (quotedMsg && quotedMsg.type == 'image') {
+                        const mediaData = await decryptMedia(quotedMsg)
+                        const imageBase64 = `data:${quotedMsg.mimetype};base64,${mediaData.toString('base64')}`
+                        await client.sendImageAsSticker(from, imageBase64)
+                    } else {
+                        client.sendText(from, 'Kirim gambar pake hashtag #sticker GOBLOK !')
+                    }
+                    break
                 case '#halo':
-                        client.sendText(from, 'Hai')
+                        client.sendText(from, 'Hai');
+                    break
+                case 'kojok':
+                        client.sendText(from, 'Kojok babi');
+                    break
+                case '#misuh':
+                        client.sendText(from, misuh[Math.floor(Math.random() * misuh.length)]);
+                    break
+                case '#quotes':
+                        client.sendText(from, quotes[Math.floor(Math.random() * quotes.length)]);
+                    break;
+                case '#quote':
+                        client.sendText(from, quotes[Math.floor(Math.random() * quotes.length)]);
                     break
             }
         } else {

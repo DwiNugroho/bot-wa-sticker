7e707634036bbe9c4e0c90f32654bee5bfb9b92b
DwiNugroho <adwinugroho1@gmail.com>
2020-11-04 20:26:23 +0700

    feat: update message

--- api/messages.js
+++ api/messages.js
@@ -1,4 +1,4 @@
 module.exports = {
   greeting:
-    "*HELLO DUDE! I'M BOT* 🤖\n\n*_Here's some Features:_*\n\n```0. #help: show helps about bot commands\n1. #halo: greetings me\n2. #sticker: return image as sticker just use caption #sticker\n3. #mentionall: will mention all of group members.\n4. #kick @user [ADMIN ONLY FOR BOTH THIS BOT AND USER]```\n\n\n\n*_HAVE A GOOD DAY_*",
+    "*HELLO DUDE! I'M BOT* 🤖\n\n*_Here's some Features:_*\n\n```0. #help: show helps about bot commands\n1. #halo: greetings me\n2. #sticker: return image as sticker just use caption #sticker\n3. #mentionall: will mention all of group members.```\n\n\n\n*_HAVE A GOOD DAY_*",
 };
